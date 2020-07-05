ea2835b3ab8f6a467cf41c79481e3f544375a20c
bbbbbbtttt <55330700+bbbbbbtttt@users.noreply.github.com>
2020-07-05 15:18:46 +0200

    Update index.ts

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,8 +1,8 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
-
+Import * as cors from 'cors'
 const serviceAccount = require('../permissions.json')
-
+Const corsHandler = cors({origin: true})
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
 	databaseURL: 'https://project-1891851661445564942.firebaseio.com'
@@ -11,7 +11,8 @@ admin.initializeApp({
 export const getFeed1 = functions.https.onRequest(async (request, response) => {
         
         const docs = await admin.firestore().collection('posts').orderBy('date', 'desc').get()
-        response.json(docs.docs.map(doc => {
+        corsHandler(request, response, () => {})
+        response.json(docs.docs.map(doc => {
             return {
                 postID: doc.id,
                 ...doc.data()
