19835b19aaf2c123cc5517c0471abe48e2b57478
Mehul Mohan <mehulmpt@gmail.com>
2020-07-07 04:09:24 +0530

    CORS Fix

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,22 +1,22 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
-Import * as cors from 'cors'
+
 const serviceAccount = require('../permissions.json')
-Const corsHandler = cors({origin: true})
+
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
 	databaseURL: 'https://project-1891851661445564942.firebaseio.com'
 })
 
-export const getFeed1 = functions.https.onRequest(async (request, response) => {
-        
-        const docs = await admin.firestore().collection('posts').orderBy('date', 'desc').get()
-        corsHandler(request, response, () => {})
-        response.json(docs.docs.map(doc => {
-            return {
-                postID: doc.id,
-                ...doc.data()
-            }
-        })
-        )
-    })
+export const getFeed1 = functions.https.onRequest(async (request, response) => {
+	const docs = await admin.firestore().collection('posts').orderBy('date', 'desc').get()
+	response.set('Access-Control-Allow-Origin', '*')
+	response.json(
+		docs.docs.map((doc) => {
+			return {
+				postID: doc.id,
+				...doc.data()
+			}
+		})
+	)
+})
