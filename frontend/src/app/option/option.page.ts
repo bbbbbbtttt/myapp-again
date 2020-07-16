1005bf23bfaca25f5d88bb72ea3cca88d2773938
bbbbbbtttt <55330700+bbbbbbtttt@users.noreply.github.com>
2020-07-16 12:42:42 +0200

    Update option.page.ts

--- frontend/src/app/option/option.page.ts
+++ frontend/src/app/option/option.page.ts
@@ -1,92 +1,119 @@
-import { Component, OnInit } from '@angular/core'
-import { Router } from '@angular/router'
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ThemeService } from '../services/theme.service';
 import { Http } from '@angular/http'
-import { AngularFirestore } from '@angular/fire/firestore'
-import { UserService } from '../user.service'
-import { firestore } from 'firebase/app'
-import { Clipboard } from '@ionic-native/clipboard/ngx'
-import { AlertController } from '@ionic/angular'
+import { AngularFirestore } from '@angular/fire/firestore';
+import { UserService } from '../user.service';
+import { firestore } from 'firebase/app';
+import { Clipboard } from '@ionic-native/clipboard/ngx';
+import { AlertController } from '@ionic/angular';
 
 @Component({
-	selector: 'app-option',
-	templateUrl: './option.page.html',
-	styleUrls: ['./option.page.scss']
+  selector: 'app-option',
+  templateUrl: './option.page.html',
+  styleUrls: ['./option.page.scss'],
 })
 export class OptionPage implements OnInit {
-	darkMode: any
+   darkMode: any;
 
-	titredeprop: string
+   
+   titredeprop: string
+  
+  date: string
+  
+  busy: boolean = false
 
-	date: string
+  prop
+  mail: 'Eloocontact@gmail.com'
+  
+  
+  constructor(private router: Router, 
+    public themeService: ThemeService,
+    public http: Http,
+    public afstore: AngularFirestore,
+    public user: UserService,
+    public clipboard: Clipboard,
+    private alertController: AlertController
+     ) { 
+      
+      this.darkMode = this.themeService.darkMode;
+    }
 
-	busy: boolean = false
+    
 
-	prop
-	mail: 'Eloocontact@gmail.com'
+  ngOnInit() {
+  }
+  CopyEmail() {
+    this.clipboard.copy(this.mail)
 
-	constructor(
-		private router: Router,
+  }
 
-		public http: Http,
-		public afstore: AngularFirestore,
-		public user: UserService,
-		public clipboard: Clipboard,
-		private alertController: AlertController
-	) {}
+  go() {
+    this.router.navigate(['edit-profile/'])
+  }
+  logout() {
+    this.router.navigate(['login/'])
+  }
+  async comptepro() {
+    const alert = await this.alertController.create({
+      header: 'bientot disponible',
+      message: 'vous avez besoin de visibiliter pour votre marque les publicités sont bientôt disponible',
+      buttons: ['Cool!']
+    })
 
-	ngOnInit() {}
-	CopyEmail() {
-		this.clipboard.copy(this.mail)
-	}
+    await alert.present()
+  }
 
-	go() {
-		this.router.navigate(['edit-profile/'])
-	}
-	logout() {
-		this.router.navigate(['login/'])
-	}
-	async comptepro() {
-		const alert = await this.alertController.create({
-			header: 'bientot disponible',
-			message:
-				'vous avez besoin de visibiliter pour votre marque les publicités sont bientôt disponible',
-			buttons: ['Cool!']
-		})
+  
+  
+  async creatprop() {
+    this.busy = true
+    
+    
+    const titredeprop = this.titredeprop
+    
+   
+    
 
-		await alert.present()
-	}
+   
 
-	async creatprop() {
-		this.busy = true
+  
 
-		const titredeprop = this.titredeprop
+    this.afstore.doc(`users/${this.user.getUID()}`).update({
+      prop: firestore.FieldValue.arrayUnion(`${titredeprop}`),
+      
+    })
 
-		this.afstore.doc(`users/${this.user.getUID()}`).update({
-			prop: firestore.FieldValue.arrayUnion(`${titredeprop}`)
-		})
+     this.afstore.doc(`prop/${titredeprop}`).set({
+         
+         
+          date: new Date().toISOString(),
+      
+      
+          author: this.user.getUsername(),
+          likes: [],
+      titredeprop
 
-		this.afstore.doc(`prop/${titredeprop}`).set({
-			date: new Date().toISOString(),
+          
+    })
 
-			author: this.user.getUsername(),
-			likes: [],
-			titredeprop
-		})
 
-		this.busy = false
+    this.busy = false
+    
+    this.titredeprop = ""
+    
+    this.date = ""
+    
+   
+    const alert = await this.alertController.create({
+      header: 'OK',
+      message: 'ta proposition est prise en compte merci pour tout!',
+      buttons: ['Cool!']
+    })
 
-		this.titredeprop = ''
+    await alert.present()
 
-		this.date = ''
+    this.router.navigate(['/tabs/profile'])
+  }
 
-		const alert = await this.alertController.create({
-			header: 'OK',
-			message: 'ta proposition est prise en compte merci pour tout!',
-			buttons: ['Cool!']
-		})
-
-		await alert.present()
-
-		this.router.navigate(['/tabs/profile'])
-	}
 }
